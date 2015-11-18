b701b2a27116da58cd95396a39ff3d6d5f1fd0a4
counterclocker <nikolaj.van.omme@gmail.com>
2015-11-18 18:03:43 -0500

    update doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -202,7 +202,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-latex_logo = 'logo/cygenja-logo64.pdf'
+latex_logo = 'logo/cygenja-logo128.pdf'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
